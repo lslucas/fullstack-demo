ae6b97260dcd052cdf806e79f90c5ddbc5b23167
Lucas Serafim <lslucas@gmail.com>
2018-06-09 13:56:28 -0500

    welcome has a about me text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,24 @@
     <div class="card-header">Dashboard</div>
 
     <div class="card-body">
-        <ul>
-            <li><a href='/github'>Github Users</a></li>
-            <li><a href='https://documenter.getpostman.com/view/178818/RWEcNfJi#8309d7fc-a6aa-4562-af52-f7bf5d917092' target='_blank'>API - Doc</a></li>
-        </ul>
+        <h4>About me</h4>
+        <p>
+            Hello, I develop with PHP since 2013~2014, started with webdesign few months before that.
+            <br/>I've worked with a vast kind of projects, since a simple CMS business website to medium port more complex ERP with customer control, finance and sellers interface.
+        </p>
+
+        <p>
+            Nowadays I work in <a href='https://fotec.mx' target='_blank'>Fotec</a> where I'm a Senior Full-Stack Developer.
+            <br/>Here I develop new endpoints for our API and implement new features to our customers. I also integrated some third-party APIs like Google Maps and made an algorith that calculate the best of our photographers for a specific order of our clients.
+        </p>
+
+	<p>
+	    Whenever I got time, when not so bored by work I improve my Python skills while playing with Machine Learning.
+	</p>
+
+	<p>
+	    I'm a brazilian, but currently I live in México. Besides natural portuguese I speak spanish fluently, my english, besides few mistakes here and there I consider OK. Spoken english I got less practice, but I can understand almost anything very well.
+	</p>
     </div>
 </div>
 @endsection
