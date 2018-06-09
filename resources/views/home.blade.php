674d2deee4d0a0195bcfc7d7460b50b41ad336c2
Lucas Serafim <lslucas@gmail.com>
2018-06-09 13:30:53 -0500

    added api docs url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="card-body">
         <ul>
             <li><a href='/github'>Github Users</a></li>
-            <li><a href='/oauth'>oAuth</a></li>
+            <li><a href='https://documenter.getpostman.com/view/178818/RWEcNfJi#8309d7fc-a6aa-4562-af52-f7bf5d917092' target='_blank'>API - Doc</a></li>
         </ul>
     </div>
 </div>
