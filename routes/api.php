9093faa59bb05ba5ce92ad8f31439d5f95c84e84
Lucas Serafim <lslucas@gmail.com>
2018-06-09 13:03:39 -0500

    Tokens should expire in 1 day now

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('authorize', 'ApiController@auth')->name('authorize');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 Route::group(
     [
